a7ddd95cfaac8b52fc650f3c94486332afa76226
tomkp <tom@tomkp.com>
2012-04-08 09:11:40 +0100

    list coercion tests

--- test/com/tomkp/nashville/coercion/ListCoercerTest.java
+++ test/com/tomkp/nashville/coercion/ListCoercerTest.java
@@ -1,7 +1,44 @@
 package com.tomkp.nashville.coercion;
 
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class ListCoercerTest {
 
 
+    private final ListCoercer coercer = new ListCoercer();
+
+
+    @Test
+    public void coerceListWithDefaultFormat() {
+        List<String> list = coercer.coerce("jim, bob, jane", null);
+        assertEquals(list.get(0), "jim");
+        assertEquals(list.get(1), "bob");
+        assertEquals(list.get(2), "jane");
+        assertEquals(list.size(), 3);
+    }
+
+
+    @Test
+    public void coerceListWithPipeFormat() {
+        List<String> list = coercer.coerce("jim|bob|jane", "\\|");
+        assertEquals(list.get(0), "jim");
+        assertEquals(list.get(1), "bob");
+        assertEquals(list.get(2), "jane");
+        assertEquals(list.size(), 3);
+    }
+
+
+    @Test
+    public void coerceListWithCommaFormat() {
+        List<String> list = coercer.coerce("jim,bob,jane", ",");
+        assertEquals(list.get(0), "jim");
+        assertEquals(list.get(1), "bob");
+        assertEquals(list.get(2), "jane");
+        assertEquals(list.size(), 3);
+    }
 
 }
